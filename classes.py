75a7ca676fcbb2aeabfd320e2e35d01d61497bff
Dhanish <dhanish265@gmail.com>
2024-12-15 21:58:52 +0800

    corner-point part 1

--- classes.py
+++ classes.py
@@ -1,4 +1,7 @@
+import copy
 import math
+
+import numpy as np
 from utils import *
 from utils import DEPTH, EXTRA_LENGTH
 
@@ -8,7 +11,7 @@ class Anchorage:
         self.vertices = vertices
         self.edges = create_edges(vertices)
         self.anchored = []
-        # print(self.edges)
+        print(self.edges)
     
     def isVesselInside(self, vessel):
         x, y = vessel.centre
@@ -26,6 +29,28 @@ class Anchorage:
         return True
     
     def generateCornerPoints(self, vessel):
+        # generate side-and-side
+        cornerPoints = []
+        for i in range(len(self.edges)):
+            edge = self.edges[i]
+            for j in range(i+1, len(self.edges)):
+                edge2 = self.edges[j]
+                if edge[0] == edge2[0] == 0:
+                    continue
+                if edge[1] == edge2[1] == 0:
+                    continue
+                if edge2[1] * edge[0] == edge[1] * edge2[0]:
+                    continue
+                A = [[edge[0], edge[1]], [edge2[0], edge2[1]]]
+                Y = [edge[-1], edge2[-1]]
+                value1 = math.sqrt(edge[0] * edge[0] + edge[1] * edge[1])
+                value2 = math.sqrt(edge2[0] * edge2[0] + edge2[1] * edge2[1])
+                Y[0] -= abs(vessel.radius * value1)
+                Y[1] -= abs(vessel.radius * value2)
+                print(A, Y)
+                res = tuple(np.linalg.inv(A).dot(Y))
+                cornerPoints.append(res)
+        return cornerPoints
         pass
         
 
@@ -52,6 +77,8 @@ vertices = [(12, -9), (-12, -9), (-12, 20)]
 anc = Anchorage(vertices)
 vessel = Vessel(2, 1, 1, (-2.5, 7.5))
 vessel2 = Vessel(0.5, 1, 1, (1.5, 7))
+cp = anc.generateCornerPoints(vessel)
+print(cp)
 # print(anc.isVesselInside(vessel))
 # print(vessel.collidesWith(vessel2))
             
