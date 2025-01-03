8d09685fc02d3ebadd4cea6cf4587df288ac5037
Dhanish <dhanish265@gmail.com>
2025-01-03 22:32:00 +0800

    read and write part 2

--- data_generator.py
+++ data_generator.py
@@ -7,6 +7,10 @@ import bisect
 from utils import MIN_IDEAL_DIST, MAX_IDEAL_DIST
 
 LIMIT = 14 * 24 * 60
+times = [None] * 3 #normal, idle, busy
+boundaries = [None] * 3
+dwell_time_raw_data = None
+# print(times)
 
 def generateData(busy_rate = 1, name = 'Synthetic Anchorage (normal)'):
     # workbook = xlsxwriter.Workbook(name  + '.xlsx')
@@ -26,7 +30,7 @@ def read_data(name):
     if not os.path.isfile("data/" + name):
         print("Incorrect file name! Please check!")
         return None
-    data = []
+    data = {}
     workbook = load_workbook("data/" + name)
     sheetNames = workbook.sheetnames
     for sheetName in sheetNames:
@@ -34,19 +38,21 @@ def read_data(name):
         worksheet = workbook[sheetName]
         for row in worksheet.values:
             values.append(row)
-        data.append(values)
+        data[sheetName] = values
     return data
 
-# data = read_data('Ahirkapi Anchorage.xlsx')
-# if data is not None:
-#     print(data[0])
+data = read_data('Ahirkapi Anchorage.xlsx')
+if data is not None:
+    print(data['100'])
 # generateData(busy_rate= 0.5, name = 'Synthetic Anchorage (idle)')
 
-def dwell_time_dist_analysis(rewrite = False):
+def dwell_time_dist_analysis(rewrite = False, busy_rate = 1, sheet_name = 'normal'):
+    global dwell_time_raw_data
     value = []
     if not rewrite:
-        data = read_data('dwell_time_analysis.xlsx')[0]
-        for row in data:
+        if dwell_time_raw_data is None:
+            dwell_time_raw_data = read_data('dwell_time_analysis.xlsx')
+        for row in dwell_time_raw_data[sheet_name]:
             value.append(row[-1])
         return value
     
@@ -54,7 +60,7 @@ def dwell_time_dist_analysis(rewrite = False):
     for _ in range(100):
         data2 = []
         for j in range(730):
-            data2.append(round(random.lognormal(2.4, 1.3) * 60))
+            data2.append(round(busy_rate * random.lognormal(2.4, 1.3) * 60))
         data2.sort()
         for j in range(730):
             # print(j)
@@ -63,7 +69,7 @@ def dwell_time_dist_analysis(rewrite = False):
         average = sum(data[j])/len(data[j])
         data[j].append(average)
         value.append(average)
-    write_to_xlsx([data], 'dwell_time_analysis.xlsx', ['normal']) 
+    write_to_xlsx([data], 'dwell_time_analysis.xlsx', [sheet_name]) 
     return value
 
 def write_to_xlsx(data, book_name: str, sheet_names: list[str]):
@@ -79,20 +85,26 @@ def write_to_xlsx(data, book_name: str, sheet_names: list[str]):
             row += 1
     workbook.close()
 
-times = dwell_time_dist_analysis(rewrite = False)
-boundaries = [(times[i] + times[i+1])/2 for i in range(len(times) - 1)]
-# print(boundaries)
-# print(times)
-# the times are obtained as [a, b, c, d, ... y, z]
-# using this, if the dwell time of a vessel is between 0 and (a+b)/2, it's ideal anchoring distance from entry side is 445
-# likewise, if it is > (y+z)/2, it's ideal anchoring distance is 2500 - 445 = 2055
-# everything in between is evenly distributed between 445 and 2055 (i.e., if dwell time is in between (a+b)/2 and (b+c)/2, 445 + (2025-445)/(26-1))
 
-def obtain_ideal_distance(dwell_time):
-    print(MIN_IDEAL_DIST + bisect.bisect(boundaries, dwell_time) * (MAX_IDEAL_DIST - MIN_IDEAL_DIST)/(len(boundaries)))
-    pass
+def instantiate_times():
+    times[0] = dwell_time_dist_analysis(rewrite = False, busy_rate=1, sheet_name='normal')
+    times[1] = dwell_time_dist_analysis(rewrite = False, busy_rate=0.5, sheet_name='idle')
+    times[2] = dwell_time_dist_analysis(rewrite = False, busy_rate=2.2, sheet_name='busy')
+    boundaries[0] = [(times[0][i] + times[0][i+1])/2 for i in range(len(times[0]) - 1)]
+    boundaries[1] = [(times[1][i] + times[1][i+1])/2 for i in range(len(times[1]) - 1)]
+    boundaries[2] = [(times[2][i] + times[2][i+1])/2 for i in range(len(times[2]) - 1)]
+    # the times are obtained as [a, b, c, d, ... y, z]
+    # using this, if the dwell time of a vessel is between 0 and (a+b)/2, it's ideal anchoring distance from entry side is 445
+    # likewise, if it is > (y+z)/2, it's ideal anchoring distance is 2500 - 445 = 2055
+    # everything in between is evenly distributed between 445 and 2055 (i.e., if dwell time is in between (a+b)/2 and (b+c)/2, 445 + (2025-445)/(26-1))
 
-obtain_ideal_distance(19.52)
+def obtain_ideal_distance(dwell_time, busy_status = 'normal'):
+    sheet_list = {'normal': 0, 'idle': 1, 'busy': 2}
+    index = sheet_list[busy_status]
+    print(MIN_IDEAL_DIST + bisect.bisect(boundaries[index], dwell_time) * (MAX_IDEAL_DIST - MIN_IDEAL_DIST)/(len(boundaries[index])))
+
+# instantiate_times()
+# obtain_ideal_distance(19.52, 'busy')
 
 
 
